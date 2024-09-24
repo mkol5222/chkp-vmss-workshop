57eb3f375176151c1e78b85d5ce86b71e0565c47
mkol5222 <mkoldov@gmail.com>
2024-09-24 12:23:55 +0000

    .

--- terraform/20-lists/list.tf
+++ terraform/20-lists/list.tf
@@ -1,7 +1,10 @@
 
 locals {
     feed1 = csvdecode(file("${abspath(path.root)}/feed1.csv"))
+
+    refeed1 = csvdecode(file("${abspath(path.root)}/refeed1.csv"))
 }
+
 
 output host1 {
   value       = local.feed1[0].DOMAIN
@@ -11,16 +14,39 @@ output path1 {
   value       = abspath(path.root)
 }
 
-resource "checkpoint_management_application_site_category" "feed1" {
-  name = "feed1"
-  description = "from feed1.csv"
-}
+# resource "checkpoint_management_application_site_category" "feed1" {
+#   name = "feed1"
+#   description = "from feed1.csv"
+# }
 
-resource "checkpoint_management_application_site" "feed1_site" {
-  name = "feed1"
-  primary_category = checkpoint_management_application_site_category.feed1.name
-  description = "domains from feed1.csv"
-  // additional_categories = ["Instant Chat", "Supports Streaming", "New Application Site Category 1",]
-  url_list = local.feed1[*].DOMAIN
-  urls_defined_as_regular_expression = false
-}
+# resource "checkpoint_management_application_site" "feed1_site" {
+#   name = "feed1"
+#   primary_category = checkpoint_management_application_site_category.feed1.name
+#   description = "domains from feed1.csv"
+#   // additional_categories = ["Instant Chat", "Supports Streaming", "New Application Site Category 1",]
+#   url_list = local.feed1[*].DOMAIN
+#   urls_defined_as_regular_expression = false
+# }
+
+# resource "checkpoint_management_application_site_category" "refeed1" {
+#   name = "refeed1"
+#   description = "from refeed1.csv"
+# }
+
+# resource "checkpoint_management_application_site" "refeed1" {
+#   name = "refeed1"
+#   primary_category = checkpoint_management_application_site_category.refeed1.name
+#   description = "domains from refeed1.csv"
+#   // url_list = local.refeed1[*].DOMAIN
+#   url_list = ["ip\\.iol\\.cz"]
+#   urls_defined_as_regular_expression = true
+# }
+
+# resource "checkpoint_management_application_site" "ifconfigme" {
+#   name = "ifconfigme"
+#   primary_category = checkpoint_management_application_site_category.refeed1.name
+#   description = "ifconfig.me"
+#   // url_list = local.refeed1[*].DOMAIN
+#   url_list = ["ifconfig\\.me"]
+#   urls_defined_as_regular_expression = true
+# }
--- terraform/20-lists/publish.tf
+++ terraform/20-lists/publish.tf
@@ -9,6 +9,10 @@ resource "checkpoint_management_publish" "example" {
   triggers = ["${timestamp()}"]
 
   depends_on = [
-    checkpoint_management_host.servac, checkpoint_management_application_site.feed1_site
+    checkpoint_management_host.servac, 
+    // checkpoint_management_application_site.feed1_site,
+  //checkpoint_management_application_site_category.refeed1,
+   //checkpoint_management_application_site.refeed1
+
   ]
 }
